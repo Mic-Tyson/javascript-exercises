f7f1e2a3fe0c3049d3e360d63f3e9596db655da5
manraj <mkooner@live.com>
2024-04-22 08:17:00 +0100

    Add solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, index) {
+    let result = "";
+    if(index<0) return "ERROR";
+    
+    for(; index>0; --index) 
+        result += str;
 
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(targetArray) {
+    const whitespace = Array.from(arguments).slice(1);
 
+    return targetArray.filter(elem => !whitespace.includes(elem));
 };
 
 // Do not edit below this line
