bec85db941abbd89646bf782d625b140cf92ac7d
manraj <mkooner@live.com>
2024-04-24 15:43:01 +0100

    Add Solutions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    array.sort((p1, p2) =>  ((p2.yearOfDeath ? p2.yearOfDeath : currentYear) - p2.yearOfBirth) - ((p1.yearOfDeath ? p1.yearOfDeath : currentYear) - p1.yearOfBirth))
+    return array[0];
 };
 
 // Do not edit below this line
