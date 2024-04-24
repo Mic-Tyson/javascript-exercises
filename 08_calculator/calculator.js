3d4c1e0afd628dcd0c6bb8a90e3bed9a286c8b4c
manraj <mkooner@live.com>
2024-04-24 12:58:59 +0100

    Add solutions to exercise 08_CALCULATOR

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,25 @@
-const add = function() {
-	
+const add = function(arg, arg1) {
+  return arg + arg1;
 };
 
-const subtract = function() {
-	
+const subtract = function(arg, arg1) {
+  return arg - arg1;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	return args.flat().reduce((total, arg) => total + arg, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.flat().reduce((total,arg) => total * arg, 1);
 };
 
-const power = function() {
-	
+const power = function(arg1, arg2) {
+	return arg1**arg2;
 };
 
-const factorial = function() {
+const factorial = function(arg) {
+  return arg === 0 ? 1 : arg*factorial(arg-1);
 	
 };
 
