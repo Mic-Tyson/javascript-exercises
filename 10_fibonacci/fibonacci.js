db5db0c207e7c913421bfe8de74b9cda70b5c235
manraj <mkooner@live.com>
2024-04-24 14:48:05 +0100

    Add recursive solution to 10_FIBONACCI

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    return  typeof n === 'string' ? fibonacci(parseInt(n)) :
+            n < 0 ? "OOPS" :
+            n ===  0 ? 0 : 
+            n === 1 ? 1 : fibonacci(n-1) + fibonacci(n-2);
 };
 
+console.log(fibonacci(2));
+console.log(fibonacci(8));
 // Do not edit below this line
 module.exports = fibonacci;
