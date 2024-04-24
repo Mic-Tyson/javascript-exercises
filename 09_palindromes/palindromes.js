44ad744f7d34848cf6f97837ada5ddf256d709b2
manraj <mkooner@live.com>
2024-04-24 13:32:46 +0100

    Add solution to exercise 09_PALINDROMES

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+    const PUNCTUATION = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ";
+    let array = arg.toLowerCase().split(''); // turn string into array so we can use methods
+    array = array.filter(elem => !PUNCTUATION.includes(elem)); // remove whitespace chars
+    let length = array.length;
+    // compare 2 sub strings from [0:MID] AND [MID:END] 
+    return array.slice(0, Math.floor(length/2)).join('') == array.slice(Math.ceil(length/2), length).reverse().join(''); 
 };
 
 // Do not edit below this line
